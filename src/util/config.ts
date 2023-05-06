8a2637a0a9c970ef9cfd47e6e3374d8b50d294c1
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-05-06 18:59:59 +0200

    add config

---
+++ src/util/config.ts
@@ -0,0 +1,7 @@
+export const nhlApi = 'https://statsapi.web.nhl.com/api/v1'
+
+export const tradesApi = 'https://trades.cyclic.app'
+
+export const logos = 'https://www-league.nhlstatic.com/images/logos/teams-current-primary-light'
+
+export const dateFormat = 'YYYY-MM-DD'
