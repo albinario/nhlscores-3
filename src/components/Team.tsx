36f7b89eb6cf781019a69adf51969e1199f3e544
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-05-11 18:45:47 +0200

    fix props definition

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,19 +1,18 @@
 import { IPlayer, ITeam } from '../interfaces'
 import { logos } from '../util/config'
 
-const Team = (
-		props: {
-			team: ITeam
-			away: boolean
-			showResults: boolean
-			score: number
-			winner: boolean
-			endType: string
-			playersPicked: IPlayer[]
-			startTime?: string
-		}
-	) => {
+interface IProps {
+	team: ITeam
+	away: boolean
+	showResults: boolean
+	score: number
+	winner: boolean
+	endType: string
+	playersPicked: IPlayer[]
+	startTime?: string
+}
 
+const Team: React.FC<IProps> = (props) => {
 	return (
 		<div className='d-flex justify-content-between'>
 			<div className='d-flex align-items-center'>
