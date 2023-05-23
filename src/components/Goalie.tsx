230f99cd6080670372bec3130b6e3d331ef32295
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-05-23 20:18:14 +0200

    fix table padding

--- src/components/Goalie.tsx
+++ src/components/Goalie.tsx
@@ -12,20 +12,20 @@ const Goalie: React.FC<IProps> = (props) => {
 
 	return (
 		<tr className={props.pickedBy}>
-			<td className='px-1 py-0'><img src={`${logos}/${props.team.id}.svg`} alt={props.team.name} /></td>
-			<td className='text-start ps-1 pe-0 py-0'>
+			<td><img src={`${logos}/${props.team.id}.svg`} alt={props.team.name} /></td>
+			<td className='text-start'>
 				<span className='small'>{props.player.jerseyNumber}</span>
 				<span className='d-none d-sm-inline'> {props.player.person.fullName}</span>
 				<span className='d-sm-none'> {props.player.person.fullName.split(' ')[1]}</span>
 				<span className='text-nowrap'> {(stats.decision) && `(${stats.decision})`}{(props.pickedBy) && ' '+props.pickedBy}</span>
 			</td>
-			<td className='px-1 py-0'>{stats.saves}/{stats.shots}</td>
-			<td className='px-1 py-0'>{stats.savePercentage.toFixed(2)}</td>
-			<td className='px-1 py-0'>{stats.powerPlayShotsAgainst-stats.powerPlaySaves}</td>
-			<td className='px-1 py-0'>{stats.goals}</td>
-			<td className='px-1 py-0'>{stats.assists}</td>
-			<td className='px-1 py-0'>{stats.pim}</td>
-			<td className='px-1 py-0'>{stats.timeOnIce}</td>
+			<td>{stats.saves}/{stats.shots}</td>
+			<td>{stats.savePercentage.toFixed(2)}</td>
+			<td>{stats.powerPlayShotsAgainst-stats.powerPlaySaves}</td>
+			<td>{stats.goals}</td>
+			<td>{stats.assists}</td>
+			<td>{stats.pim}</td>
+			<td>{stats.timeOnIce}</td>
 		</tr>
 	)
 }
