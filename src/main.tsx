75d61b3ae20b28935570b9af3c6cd24f4cb278e2
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-11-11 14:38:08 +0100

    react query 5 change

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ const queryClient = new QueryClient({
 		queries: {
 			refetchOnWindowFocus: false,
 			staleTime: 1000 * 60 * 15,
-			cacheTime: 1000 * 60 * 60,
+			gcTime: 1000 * 60 * 60,
 		},
 	},
 })
