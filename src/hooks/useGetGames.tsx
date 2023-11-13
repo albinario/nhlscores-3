65b37b0266c051293aa3c405ee990d02a9e23c1d
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-11-13 23:04:04 +0100

    shorten hooks

--- src/hooks/useGetGames.tsx
+++ src/hooks/useGetGames.tsx
@@ -1,9 +1,8 @@
 import { useQuery } from '@tanstack/react-query'
 import { getGames } from '../services/NhlAPI'
 
-export const useGetGames = (date: string) => {
-	return useQuery({
+export const useGetGames = (date: string) =>
+	useQuery({
 		queryKey: ['games', date],
 		queryFn: () => getGames(date),
 	})
-}
